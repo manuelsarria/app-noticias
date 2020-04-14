ea275f1216cb8fbfb3dfc48aec427f1018a528eb
manuel sarria <manuell.sarria@gmail.com>
2020-04-14 10:22:45 -0500

    toast add and delete newspaper

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -15,6 +15,7 @@ export class NoticiaComponent implements OnInit {
 
   @Input() noticia: Article;
   @Input() indice: number;
+  @Input() enFavoritos;
 
   constructor(private iab: InAppBrowser,
               private actionSheetCtrl: ActionSheetController,
@@ -31,6 +32,33 @@ export class NoticiaComponent implements OnInit {
   }
 
   async lanzarMenu() {
+
+    let guardarBorrarBtn;
+
+    if ( this.enFavoritos ) {
+       // borrar de favoritos
+       guardarBorrarBtn = {
+        text: 'Borrar Favorito',
+        icon: 'trash',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Borrar de favorito1');
+          this.dataLocalService.borrarNoticia(this.noticia);
+        }
+      };
+
+    } else {
+      guardarBorrarBtn = {
+        text: 'Favorito',
+        icon: 'star',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('favorito');
+          this.dataLocalService.guardarNoticia(this.noticia);
+        }
+      };
+    }
+
     const actionSheet = await this.actionSheetCtrl.create({
       buttons: [
         {
@@ -46,15 +74,9 @@ export class NoticiaComponent implements OnInit {
             this.noticia.url
           );
         }
-      }, {
-        text: 'Favorito',
-        icon: 'star',
-        cssClass: 'action-dark',
-        handler: () => {
-          console.log('favorito');
-          this.dataLocalService.guardarNoticia(this.noticia);
-        }
-      }, {
+      },
+      guardarBorrarBtn,
+      {
         text: 'Cancelar',
         icon: 'close',
         role: 'cancel',
--- src/app/services/data-local.service.ts
+++ src/app/services/data-local.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { Article } from '../interfaces/interfaces';
+import { ToastController } from '@ionic/angular';
 
 
 @Injectable({
@@ -10,7 +11,20 @@ export class DataLocalService {
 
   noticias: Article[] = [];
 
-  constructor( private storage: Storage) { }
+  constructor( private storage: Storage,
+               private toastCtrl: ToastController) {
+
+    this.cargarFavoritos();
+  }
+
+  async presentToast( message: string) {
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 1500
+    });
+    toast.present();
+  }
+
 
   guardarNoticia( noticia: Article) {
 
@@ -20,12 +34,29 @@ export class DataLocalService {
 
       this.noticias.unshift( noticia);
       this.storage.set('favoritos', this.noticias);
+    }
 
+    this.presentToast( 'Agregado a Favoritos');
+  }
+
+  async cargarFavoritos() {
+
+    const favoritos = await this.storage.get('favoritos');
+      // .then( favoritos => {
+      //   console.log('favoritos', favoritos);
+      // });
+
+    if ( favoritos ) {
+
+      this.noticias = favoritos;
     }
 
   }
 
-  cargarFavoritos() {
+  borrarNoticia( noticia: Article) {
 
+    this.noticias = this.noticias.filter(noti => noti.title !== noticia.title );
+    this.storage.set('favoritos', this.noticias);
+    this.presentToast( 'Borrado de Favoritos' );
   }
 }
